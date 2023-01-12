a1702b57be21a06a67f141038ebbbb6253d66a76
Aditia Prasetio <aditia.prasetio@qbit.com>
2023-01-12 15:49:07 +0700

    get bormar after sign in

--- src/screens/Auth/SignInWithKompas.tsx
+++ src/screens/Auth/SignInWithKompas.tsx
@@ -2,40 +2,25 @@ import React, {useEffect, useState} from 'react';
 import {Spinner} from 'native-base';
 import {View} from 'react-native';
 import WebView from 'react-native-webview';
-import {AuthService} from '../../api/auth.service';
-
-const injectScript = `
-  (function () {
-    window.onclick = function(e) {
-      e.preventDefault();
-      window.postMessage(e.target.href);
-      e.stopPropagation()
-    }
-  }());
-`;
 
 export default function SignInWithKompas() {
-  const [uri, setUri] = useState<string>();
+  // const [uri, setUri] = useState<string>();
 
   useEffect(() => {
-    AuthService.bindMemberToKompas().then(res => {
-      setUri(res.url);
-    });
+    // AuthService.bindMemberToKompas().then(res => {
+    //   setUri(res.url);
+    // });
   }, []);
 
   return (
     <View style={{flex: 1}}>
-      {uri ? (
-        <WebView
-          onNavigationStateChange={state => console.info('state', state)}
-          // injectedJavaScript={injectScript}
-          source={{uri: uri || ''}}
-          userAgent="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
-          originWhitelist={['https://*', 'http://*', 'file://*', 'sms://*']}
-        />
-      ) : (
-        <Spinner />
-      )}
+      {/* <WebView
+        onNavigationStateChange={state => console.info('state', state)}
+        // injectedJavaScript={injectScript}
+        source={{uri: uri || ''}}
+        userAgent="Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko"
+        originWhitelist={['https://*', 'http://*', 'file://*', 'sms://*']}
+      /> */}
     </View>
   );
 }
--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import {useNavigation, useRoute} from '@react-navigation/native';
 import {Box, Button} from 'native-base';
 import React, {useEffect, useState} from 'react';
 import {Alert, Linking} from 'react-native';
 import {AuthService} from '../../api/auth.service';
 import {getErrorMessage} from '../../helpers/errorHandler';
+import httpRequest from '../../helpers/httpRequest';
 
 export default function AuthScreen() {
   // const _resDLI = useDeeplinkInit();
@@ -23,6 +25,22 @@ export default function AuthScreen() {
     });
   }, []);
 
+  useEffect(() => {
+    if (params && params.authorization_code) {
+      httpRequest
+        .get(
+          'https://my.borobudurmarathon.com/dev.titudev.com/api/v1/kompasid/login/auth?authorization_code' +
+            params.authorization_code,
+        )
+        .then(res => {
+          console.info('res.data', res.data);
+        })
+        .catch(err => {
+          console.info('error', err);
+        });
+    }
+  }, [route.params]);
+
   const redirect_uri = 'bormar://auth-me';
   const url =
     'https://account.kompas.id/sso/check?client_id=3&state=borobudur_marathon&scope=nama%20lengkap,%20alamat,%20Alamat%20email%20dan%20mengirimkan%20pesan&response_type=code&redirect_uri=' +
@@ -34,12 +52,10 @@ export default function AuthScreen() {
     try {
       Linking.openURL(url);
       return;
-
     } catch (error) {
       Alert.alert(getErrorMessage(error));
     }
   };
-
 
   return (
     <Box>

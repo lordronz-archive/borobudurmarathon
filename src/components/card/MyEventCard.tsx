ee6ecdc80999ce7841039f0f83d22df2cfed8ebb
LordRonz <aarontanhar2000@gmail.com>
2023-01-25 01:14:25 +0700

    feat: add my events screen integration :rocket:

--- src/components/card/MyEventCard.tsx
+++ src/components/card/MyEventCard.tsx
@@ -1,39 +1,42 @@
-import {
-  HStack,
-  Image,
-  VStack,
-  Text,
-  Box,
-  AspectRatio,
-  Stack,
-  Badge,
-  Alert,
-} from 'native-base';
+import moment from 'moment';
+import {HStack, VStack, Text, Box, Stack, Badge, Alert} from 'native-base';
 import React from 'react';
-import {ImageSourcePropType} from 'react-native';
 import IconRun from '../../assets/icons/IconRun';
 import IconTag from '../../assets/icons/IconTag';
 import MButton from '../buttons/Button';
 
 type MyCardEventProps = {
   title: string;
-  place: string;
+  paid: boolean;
   date: string;
-  image: ImageSourcePropType;
+  transactionExpirationTime?: string;
   isAvailable?: boolean;
 };
 
+enum PAYMENT_STATUS {
+  REGISTERED,
+  WAITING_PAYMENT,
+  PAID,
+  UNQUALIFIED,
+  PAYMENT_EXPIRED,
+}
+
 export default function MyCardEvent({
   title,
-  place,
   date,
-  image,
-  isAvailable = true,
+  paid,
+  transactionExpirationTime,
 }: MyCardEventProps) {
+  const status = paid
+    ? PAYMENT_STATUS.PAID
+    : moment().isBefore(moment(transactionExpirationTime))
+    ? PAYMENT_STATUS.WAITING_PAYMENT
+    : PAYMENT_STATUS.PAYMENT_EXPIRED;
+
   return (
     <Box alignItems="flex-start" my={3} width="100%">
       <HStack flex={1}>
-        <Stack pl={3} flexGrow={1}>
+        <Stack pl={3} flexGrow={1} space="2">
           <HStack flex="1" justifyContent={'space-between'} space="1">
             <Text
               flex={'1'}
@@ -44,57 +47,93 @@ export default function MyCardEvent({
               flexWrap={'wrap'}>
               {title}
             </Text>
-            {!isAvailable && (
+            {status === PAYMENT_STATUS.PAID ? (
               <Badge
-                backgroundColor="gray.200"
+                backgroundColor="#DFF4E0"
                 px="3"
                 py="0.5"
                 borderRadius="4"
                 alignSelf="flex-start"
                 _text={{
-                  color: 'gray.500',
+                  color: '#26A62C',
                   fontWeight: 'bold',
                   fontSize: 'xs',
                 }}>
-                Expired Event
+                Paid
               </Badge>
-            )}
+            ) : status === PAYMENT_STATUS.PAYMENT_EXPIRED ? (
+              <Badge
+                backgroundColor="#FDEBEB"
+                px="3"
+                py="0.5"
+                borderRadius="4"
+                alignSelf="flex-start"
+                _text={{
+                  color: '#FE4545',
+                  fontWeight: 'bold',
+                  fontSize: 'xs',
+                }}>
+                Payment Expired
+              </Badge>
+            ) : status === PAYMENT_STATUS.WAITING_PAYMENT ? (
+              <Badge
+                backgroundColor="#FFF8E4"
+                px="3"
+                py="0.5"
+                borderRadius="4"
+                alignSelf="flex-start"
+                _text={{
+                  color: '#A4660A',
+                  fontWeight: 'bold',
+                  fontSize: 'xs',
+                }}>
+                Waiting Payment
+              </Badge>
+            ) : null}
           </HStack>
           <HStack flex="1" justifyContent={'space-between'}>
             <HStack space={1} alignItems="center">
               <IconRun />
               <Text fontSize="xs" color="coolGray.500">
-                Nov 28 - Dec 07 2022
+                {date}
               </Text>
             </HStack>
             <HStack space={2} alignItems="center">
               <IconTag />
-              <Text fontSize="xs" color="coolGray.800">
+              <Text fontSize="xs" color="coolGray.500">
                 Elite Runner 42 Km
               </Text>
             </HStack>
           </HStack>
-          <HStack mt="2" space="2">
-            <Alert
-              status="warning"
-              bgColor={'#FFF8E4'}
-              borderRadius={8}
-              flexGrow={1}>
-              <VStack space={2} flexShrink={1} w="100%">
-                <HStack flexShrink={1} space={2} justifyContent="space-between">
-                  <HStack space={2} flexShrink={1} alignItems="center">
-                    <Alert.Icon mt="1" />
-                    <Text fontSize={12} color="coolGray.800">
-                      Pay before 02 Oct 2022, 14:32
-                    </Text>
+          {status === PAYMENT_STATUS.WAITING_PAYMENT && (
+            <HStack space="2">
+              <Alert
+                status="warning"
+                bgColor={'#FFF8E4'}
+                borderRadius={8}
+                flexGrow={1}>
+                <VStack space={2} flexShrink={1} w="100%">
+                  <HStack
+                    flexShrink={1}
+                    space={2}
+                    justifyContent="space-between">
+                    <HStack space={2} flexShrink={1} alignItems="center">
+                      <Alert.Icon mt="1" />
+                      <Text fontSize={12} color="coolGray.800">
+                        Pay before{' '}
+                        {moment(transactionExpirationTime).format(
+                          'DD MMM YYYY, HH:MM',
+                        )}
+                      </Text>
+                    </HStack>
                   </HStack>
-                </HStack>
-              </VStack>
-            </Alert>
-            <MButton variant="outline" _text={{fontSize: 12}}>
-              Pay Now
-            </MButton>
-          </HStack>
+                </VStack>
+              </Alert>
+              <MButton variant="outline" _text={{fontSize: 12}}>
+                Pay Now
+              </MButton>
+            </HStack>
+          )}
         </Stack>
       </HStack>
     </Box>

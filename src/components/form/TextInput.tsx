df4bf16ca083605505027a1d590f6d9e45fb683f
LordRonz <aarontanhar2000@gmail.com>
2023-03-09 22:33:02 +0700

    feat: adjust register event form :rocket:

--- src/components/form/TextInput.tsx
+++ src/components/form/TextInput.tsx
@@ -1,4 +1,11 @@
-import {Box, FormControl, Input, WarningOutlineIcon} from 'native-base';
+import {
+  Box,
+  FormControl,
+  HStack,
+  Input,
+  Text,
+  WarningOutlineIcon,
+} from 'native-base';
 import {IInputProps} from 'native-base/lib/typescript/components/primitives/Input/types';
 import React from 'react';
 
@@ -9,6 +16,7 @@ type TextInputProps = {
   placeholder?: string;
   label?: string;
   value?: string;
+  required?: boolean;
   _inputProps?: IInputProps;
   onChangeText?: (text: string) => void;
 };
@@ -17,7 +25,17 @@ export default function TextInput(props: TextInputProps) {
   return (
     <FormControl isInvalid={props.isInvalid}>
       <Box borderWidth={1} borderColor="#C5CDDB" borderRadius={5} px={3} pb={2}>
-        <FormControl.Label>{props.label}</FormControl.Label>
+        <FormControl.Label flexWrap={'wrap'}>
+          <HStack>
+            {props.required && <Text color="primary.900">* </Text>}
+            <Text>{props.label}</Text>
+          </HStack>
+          {props.required && (
+            <Text color="primary.900" italic ml="2">
+              Required
+            </Text>
+          )}
+        </FormControl.Label>
         <Input
           placeholder={props.placeholder}
           variant="unstyled"

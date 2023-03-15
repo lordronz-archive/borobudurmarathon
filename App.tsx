c142e4a99fe9beed18da0651882c206f7b7b8c30
Aditia Prasetio <aditia.prasetio@qbit.com>
2023-03-15 17:43:21 +0700

    adjust

--- App.tsx
+++ App.tsx
@@ -27,6 +27,7 @@ import id from './src/lib/i18n/id';
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
+    compatibilityJSON: 'v3',
     // the translations
     // (tip move them in a JSON file and import them,
     // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
